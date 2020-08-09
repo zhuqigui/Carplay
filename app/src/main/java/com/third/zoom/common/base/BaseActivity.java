e4907332a3fe34da694c156fc32b4c30b5c4c249
zhuqg <369793561@qq.com>
2020-08-10 07:04:27 +0800

    add copy folder method

--- app/src/main/java/com/third/zoom/common/base/BaseActivity.java
+++ app/src/main/java/com/third/zoom/common/base/BaseActivity.java
@@ -1,9 +1,16 @@
 package com.third.zoom.common.base;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+import com.third.zoom.common.utils.FileUtils;
+import com.third.zoom.common.utils.PreferenceUtils;
 
 /**
  * 作者：Sky on 2018/3/5.
@@ -11,9 +18,10 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public abstract class BaseActivity extends AppCompatActivity {
-
+    private static final String TAG="zhu_test";
     protected ActivityFragmentInject annotation;
-
+    FileUtils mFileUtils;
+    private ProgressDialog progressDialog;
     protected Handler mHandler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
@@ -37,6 +45,7 @@ public abstract class BaseActivity extends AppCompatActivity {
         setContentView(annotation.contentViewId());
         findViewAfterViewCreate();
         initDataAfterFindView();
+        mFileUtils=new FileUtils();
     }
 
     @Override
@@ -53,5 +62,58 @@ public abstract class BaseActivity extends AppCompatActivity {
      * 数据初始化
      */
     protected abstract void initDataAfterFindView();
-
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        Log.i(TAG,"BaseActivity onKeyDown keyCode=="+keyCode);
+        if(keyCode==14){
+            Log.i("zhu_test","keyCode==14 start copy media");
+            //mFileUtils.copyMedia();
+            new Thread(new Runnable(){
+                @Override
+                public void run() {
+                    try {
+                        mFileUtils.copyMedia();
+                        progressDialog.dismiss();//关闭进程对话框
+                        Toast.makeText(BaseActivity.this,"拷贝视频已完成",Toast.LENGTH_LONG).show();
+                        //runOnUiThread(finishDialog);//要求运行在UI线程
+                    } catch (Exception e) {}
+                }
+            }).start();
+            progressDialog= ProgressDialog.show(BaseActivity.this, "请稍等", "正在拷贝视频中，请不要插拔U盘或SD卡...", true);
+        }else if(keyCode==15){
+            Log.i(TAG,"keyCode==15 start copy music");
+            //mFileUtils.copyMedia();
+            new Thread(new Runnable(){
+                @Override
+                public void run() {
+                    try {
+                        mFileUtils.copyMusic();
+                        progressDialog.dismiss();//关闭进程对话框
+                        Toast.makeText(BaseActivity.this,"拷贝音乐已完成",Toast.LENGTH_LONG).show();
+                        //runOnUiThread(finishDialog);//要求运行在UI线程
+                    } catch (Exception e) {}
+                }
+            }).start();
+            progressDialog= ProgressDialog.show(BaseActivity.this, "请稍等", "正在拷贝音乐中，请不要插拔U盘或SD卡...", true);
+        }else if(keyCode==16){
+            Log.i(TAG,"keyCode==16 start copy photo");
+            //mFileUtils.copyMedia();
+            new Thread(new Runnable(){
+                @Override
+                public void run() {
+                    try {
+                        mFileUtils.copyPhoto();
+                        progressDialog.dismiss();//关闭进程对话框
+                        Toast.makeText(BaseActivity.this,"拷贝图片已完成",Toast.LENGTH_LONG).show();
+                        //runOnUiThread(finishDialog);//要求运行在UI线程
+                    } catch (Exception e) {}
+                }
+            }).start();
+            progressDialog= ProgressDialog.show(BaseActivity.this, "请稍等", "正在拷贝图片中，请不要插拔U盘或SD卡..", true);
+        }else if(keyCode==82){
+            //menu键退出系统
+            System.exit(0);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
--- app/src/main/java/com/third/zoom/common/utils/FileUtils.java
+++ app/src/main/java/com/third/zoom/common/utils/FileUtils.java
@@ -1,10 +1,17 @@
 package com.third.zoom.common.utils;
 
 import android.os.Environment;
+import android.util.Log;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import java.util.Date;
 
 /**
  * 作者：Sky on 2018/9/4.
@@ -13,6 +20,20 @@ import java.io.IOException;
 
 public class FileUtils {
 
+    private final static int COPY_FAIL = -1;
+    private final static int COPY_SUCCESS = 0;
+    //private final static int COPY_START = 1;
+    private final static String FROMPATH_MEDIA_UDISK = "/storage/udisk3/VIDEO";
+    private final static String FROMPATH_MUSIC_UDISK = "/storage/udisk3/MUSIC";
+    private final static String FROMPATH_PHOTO_UDISK = "/storage/udisk3/PHOTO";
+    private final static String TOPATH_MEDIA_UDISK = "/storage/card/VIDEO/";
+    private final static String TOPATH_MUSIC_UDISK = "/storage/card/MUSIC/";
+    private final static String TOPATH_PHOTO_UDISK = "/storage/card/PHOTO/";
+
+    Copyer copyer;
+    public FileUtils(){
+        copyer = new Copyer();
+    }
     /**
      * 保存错误记录
      * @param msg
@@ -46,4 +67,106 @@ public class FileUtils {
         }
     }
 
+    private void startCopy(final String fromPath, final String toPath) {
+        //mHandler.sendEmptyMessage(COPY_START);
+        //mCopyStartBtn.setClickable(false);
+//        new Thread() {
+//            public void run() {
+                if (copy(fromPath, toPath) == COPY_SUCCESS) {
+                    //mHandler.sendEmptyMessage(COPY_SUCCESS);
+                    Log.i("zhu_test","copy success...");
+                } else {
+                    //mHandler.sendEmptyMessage(COPY_FAIL);
+                }
+            //}
+//        }.start();
+    }
+
+    /**
+     * 拷贝函数
+     * @param fromFile 文件来源路径
+     * @param toFile 拷贝到的路径
+     * @return
+     */
+    public int copy(String fromFile, String toFile) {
+        long start=System.currentTimeMillis();
+        File[] currentFiles;
+        File root = new File(fromFile);
+        if (!root.exists()) {
+            Log.i("zhu_test","U disk files no exists...");
+            return COPY_FAIL;
+        }
+        currentFiles = root.listFiles();
+        File targetDir = new File(toFile);
+
+        if (!targetDir.exists()) {
+            targetDir.mkdirs();
+        }
+        for (int i = 0; i < currentFiles.length; i++) {
+            Log.i("zhu_test","currentFiles[i].isDirectory()=="+currentFiles[i].isDirectory());
+            if (currentFiles[i].isDirectory())
+            {
+                copy(currentFiles[i].getPath() + "/",
+                        toFile + currentFiles[i].getName() + "/");
+            }
+            else
+            {
+                copyFile(currentFiles[i].getPath(), toFile
+                        + currentFiles[i].getName());
+                //copyer.copy(currentFiles[i],toFile,10);
+
+                //copyFile1(currentFiles[i].getPath(), toFile
+                //        + currentFiles[i].getName());
+                //Log.i("zhu_test","copy file spend time=="+(System.currentTimeMillis()-start));
+            }
+        }
+        return COPY_SUCCESS;
+    }
+    public void copyFile1(String fromFile, String toFile){
+        long filelength = new File(fromFile).length();
+        long size = filelength/5;
+//        Date date = new Date();
+//        Log.i("zhu_test","文件拷贝开始时间为："+date+"\n 正在拷贝文件······");
+        //定义5个线程
+        for (int i = 0; i < 5; i++) {
+            new ThreadCopyFile(fromFile,toFile,i*size,(i+1)*size).start();
+        }
+//		Thr
+    }
+    /**
+     * 拷贝单一文件
+     * @param fromFile 文件来源路径
+     * @param toFile 拷贝到的路径
+     * @return
+     */
+    public int copyFile(String fromFile, String toFile) {
+        try {
+            Log.i("zhu_test","copyFile fromFile=="+fromFile+",toFile=="+toFile);
+            InputStream fosfrom = new FileInputStream(fromFile);
+            OutputStream fosto = new FileOutputStream(toFile);
+            byte bt[] = new byte[1024];
+            int c;
+            while ((c = fosfrom.read(bt)) > 0) {
+                fosto.write(bt, 0, c);
+            }
+            fosfrom.close();
+            fosto.close();
+//            currentSize += (new File(fromFile)).length();
+//            mHandler.sendEmptyMessage(CURRENT_SIZE);
+            Log.i("zhu_test","copyFile copy success...");
+            return COPY_SUCCESS;
+        } catch (Exception ex) {
+            Log.i("zhu_test","copyFile copy fail..."+ex.getLocalizedMessage());
+            return COPY_FAIL;
+        }
+    }
+    public void copyMedia(){
+        startCopy(FROMPATH_MEDIA_UDISK,TOPATH_MEDIA_UDISK);
+    }
+    public void copyMusic(){
+        startCopy(FROMPATH_MUSIC_UDISK,TOPATH_MUSIC_UDISK);
+    }
+    public void copyPhoto(){
+        startCopy(FROMPATH_PHOTO_UDISK,TOPATH_PHOTO_UDISK);
+    }
 }
